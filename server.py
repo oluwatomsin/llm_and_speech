455ba18b4b15573079d42619b365e2cfc190c54a
TomiAmusa <amusatomisin65@gmail.com>
2024-09-03 13:51:30 +0100

    Updated the logging, llm summarization process to support file upload

--- server.py
+++ server.py
@@ -1,12 +1,19 @@
 from dotenv import load_dotenv
 from engine.audio_synthesis import get_audio
-from engine.chat_utils import get_response
-import os
-
+from engine.chat_utils import get_summary
+from engine.utils import logger
 
 load_dotenv()
-# Using OpenAI GPT4o
 
 
-text = get_response(message="")
-get_audio(text=text)
+def main(path='doc.txt'):
+        logger.info("Starting the Vocal Summarization Process...")
+        logger.info("Step 1. Attempting to get summarization")
+        text = get_summary(file_path=path)
+        logger.info("Step 2. Attempting to generate audio")
+        get_audio(text=text)
+        logger.info('Process Completed Successfully.')
+
+
+if __name__ == '__main__':
+        main()
